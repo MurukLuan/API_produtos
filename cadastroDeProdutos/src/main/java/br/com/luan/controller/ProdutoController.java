c26253214254f51f6a3541040d38263da382678b
murukluan <murukluan@gmail.com>
2023-09-28 17:11:45 -0300

    projeto finalizado

--- cadastroDeProdutos/src/main/java/br/com/luan/controller/ProdutoController.java
+++ cadastroDeProdutos/src/main/java/br/com/luan/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,5 +66,17 @@ public class ProdutoController {
 					return ResponseEntity.ok().body(produtoAtualizado);
 				}).orElse(ResponseEntity.notFound().build());
 	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletarProduto(@PathVariable Long id ) {
+		return produtoRepository.findById(id)
+		.map(objetoGravado -> {
+			produtoRepository.deleteById(id);
+			return ResponseEntity.noContent().<Void>build();
+		}).orElse(ResponseEntity.notFound().build());
+	}
+	
+	
+	
 	
 }
